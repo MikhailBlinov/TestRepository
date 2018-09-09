2b46865d3f704c070c5813f37379d850489e4834
Mikhail Blinov <ims_mike@mail.ru>
2018-09-09 14:08:09 +0300

    The commit

---
+++ CSharpNewConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpNewConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is a C# Console Project");
+            Console.Read();
+        }
+    }
+}
